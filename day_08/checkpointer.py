5ff8a7743e169bebceb22c0e860017bf48b7bcdf
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-09-12 09:35:44 +0500

    Checkpointer implemented

--- day_08/checkpointer.py
+++ day_08/checkpointer.py
@@ -16,3 +16,81 @@ def chatbot(state: MessagesState):
             )
         ]
     }
+
+
+builder = StateGraph(MessagesState)
+
+builder.add_node(
+    "chatbot",
+    chatbot
+)
+
+builder.add_edge(
+    START,
+    "chatbot"
+)
+
+builder.add_edge(
+    "chatbot",
+    END
+)
+
+
+checkpointer = InMemorySaver()
+
+
+app = builder.compile(
+    checkpointer=checkpointer
+)
+
+
+config = {
+    "configurable": {
+        "thread_id": "user_1"
+    }
+}
+
+
+result = app.invoke(
+    {
+        "messages": [
+            HumanMessage(
+                content="My name is Noman."
+            )
+        ]
+    },
+    config
+)
+
+
+print("\nFIRST RESPONSE:")
+print(result["messages"][-1].content)
+
+
+result = app.invoke(
+    {
+        "messages": [
+            HumanMessage(
+                content="What did I just say?"
+            )
+        ]
+    },
+    config
+)
+
+
+print("\nSECOND RESPONSE:")
+print(result["messages"][-1].content)
+
+
+state = app.get_state(config)
+
+
+print("\nSAVED STATE:")
+print(state.values)
+
+
+print("\nCHECKPOINT HISTORY:")
+
+for checkpoint in app.get_state_history(config):
+    print(checkpoint)
