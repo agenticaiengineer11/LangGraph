0bb2e6ab26ea2ed85d3070f15726c55b2875d790
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-09-13 10:48:39 +0500

    parent graph with subgraph implemented

--- day_11/main.py
+++ day_11/main.py
@@ -6,14 +6,17 @@ class State(TypedDict):
     result: str
 
 def analyzer(state: State):
-    print("\n------Analyzer running-----")
+
+    print("Analyzer running...")
 
     return {
         "result": f"Analyzed: {state['input']}"
     }
 
+
 def processor(state: State):
-    print("\n----- Processor running----")
+
+    print("Processor running...")
 
     return {
         "result": f"Processed: {state['result']}"
@@ -21,24 +24,98 @@ def processor(state: State):
 
 subgraph_builder = StateGraph(State)
 
-subgraph_builder.add_node("analyzer", analyzer)
-subgraph_builder.add_node("processor", processor)
+subgraph_builder.add_node(
+    "analyzer",
+    analyzer
+)
 
-subgraph_builder.add_edge(START , "analyzer")
+subgraph_builder.add_node(
+    "processor",
+    processor
+)
 
-subgraph_builder.add_edge("analyzer", "processor")
+subgraph_builder.add_edge(
+    START,
+    "analyzer"
+)
 
-subgraph_builder.add_edge("processor", END)
+subgraph_builder.add_edge(
+    "analyzer",
+    "processor"
+)
+
+subgraph_builder.add_edge(
+    "processor",
+    END
+)
 
 subgraph = subgraph_builder.compile()
 
-result = subgraph.invoke(
-    {
-        "input" : "Langgraph",
-        "result" : " "    
+def request_node(state: State):
 
+    print("Request node running...")
+
+    return {
+        "input": state["input"]
+    }
+
+
+def final_response(state: State):
+
+    print("Final response node running...")
+
+    return {
+        "result": f"Final result: {state['result']}"
+    }
+parent_builder = StateGraph(State)
+
+
+parent_builder.add_node(
+    "request",
+    request_node
+)
+
+parent_builder.add_node(
+    "processing",
+    subgraph
+)
+
+
+parent_builder.add_node(
+    "final_response",
+    final_response
+)
+
+parent_builder.add_edge(
+    START,
+    "request"
+)
+
+parent_builder.add_edge(
+    "request",
+    "processing"
+)
+
+parent_builder.add_edge(
+    "processing",
+    "final_response"
+)
+
+parent_builder.add_edge(
+    "final_response",
+    END
+)
+
+
+parent_app = parent_builder.compile()
+
+result = parent_app.invoke(
+    {
+        "input": "LangGraph",
+        "result": ""
     }
 )
-print("\n =========Final Result=========")
-print(result)
 
+print("\n========== FINAL RESULT ==========")
+
+print(result)
