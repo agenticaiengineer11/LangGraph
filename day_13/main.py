82fd7c485043161e7f572ffe27c166cf28b32555
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-09-19 10:46:02 +0500

    Fan in and fan out files started for practice

---
+++ day_13/main.py
@@ -0,0 +1,26 @@
+from typing import TypedDict
+
+
+class AgentState(TypedDict):
+    user_query: str
+    research: str
+    competitors: str
+    market: str
+def research_agent(state: AgentState):
+    print("\n -----research agent----------")
+    return{
+        "research": (
+            f"Findings research for: "
+            f"{state['user_query']}"
+        )
+    }
+
+def competitor_agent(state:AgentState):
+    print("\n -----competitor agent---------")
+
+    return {
+        "competitors": (
+            f"Competitor analysis for: "
+            f"{state['user_query']}"
+        )
+    }
