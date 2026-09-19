8c531b75aa8c4ef4da6028dbf0d42a969bc48241
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-09-19 11:16:04 +0500

    reducers used for same shared state

---
+++ day_13/reducers.py
@@ -0,0 +1,121 @@
+from typing import TypedDict, Annotated
+import operator
+
+from langgraph.graph import StateGraph, START, END
+
+class AgentState(TypedDict):
+    user_query: str
+    findings: Annotated[list[str], operator.add]
+
+def research_agent(state: AgentState):
+    print("\n----- RESEARCH AGENT -----")
+
+    return {
+        "findings": [
+            f"Research findings for: {state['user_query']}"
+        ]
+    }
+
+
+def competitor_agent(state: AgentState):
+    print("\n----- COMPETITOR AGENT -----")
+
+    return {
+        "findings": [
+            f"Competitor analysis for: {state['user_query']}"
+        ]
+    }
+
+
+def market_agent(state: AgentState):
+    print("\n----- MARKET AGENT -----")
+
+    return {
+        "findings": [
+            f"Market analysis for: {state['user_query']}"
+        ]
+    }
+
+def aggregator(state: AgentState):
+    print("\n----- AGGREGATOR -----")
+
+    combined = "\n\n".join(
+        state["findings"]
+    )
+
+    print("\nCombined findings:")
+    print(combined)
+
+    return {}
+
+builder = StateGraph(AgentState)
+
+builder.add_node(
+    "research_agent",
+    research_agent
+)
+
+builder.add_node(
+    "competitor_agent",
+    competitor_agent
+)
+
+builder.add_node(
+    "market_agent",
+    market_agent
+)
+
+builder.add_node(
+    "aggregator",
+    aggregator
+)
+
+
+builder.add_edge(
+    START,
+    "research_agent"
+)
+
+builder.add_edge(
+    START,
+    "competitor_agent"
+)
+
+builder.add_edge(
+    START,
+    "market_agent"
+)
+
+builder.add_edge(
+    "research_agent",
+    "aggregator"
+)
+
+builder.add_edge(
+    "competitor_agent",
+    "aggregator"
+)
+
+builder.add_edge(
+    "market_agent",
+    "aggregator"
+)
+
+builder.add_edge(
+    "aggregator",
+    END
+)
+
+app = builder.compile()
+
+result = app.invoke({
+    "user_query": "Analyze the AI agent market",
+    "findings": []
+})
+
+print("\n========================================")
+print("             FINAL FINDINGS")
+print("========================================")
+
+for finding in result["findings"]:
+    print(f"\n- {finding}")
