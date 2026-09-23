834de6bc5baedf6ad08a69544ddbfde6c252c0e9
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-09-23 08:48:50 +0500

    llm tracing implemented and tested

---
+++ day_14/tests/llm_tracing.py
@@ -0,0 +1,46 @@
+import os
+
+from dotenv import load_dotenv
+from typing import TypedDict
+
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, START, END
+
+load_dotenv()
+
+
+class State(TypedDict):
+    question: str
+    answer: str
+
+model = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0
+)
+
+
+def llm_agent(state: State):
+
+    response = model.invoke(state["question"])
+
+    return {
+        "answer": response.content
+    }
+
+builder = StateGraph(State)
+
+builder.add_node("llm_agent", llm_agent)
+
+builder.add_edge(START, "llm_agent")
+builder.add_edge("llm_agent", END)
+
+app = builder.compile()
+
+result = app.invoke({
+    "question": "What is an AI agent?",
+    "answer": ""
+})
+
+
+print("Final answer:")
+print(result["answer"])
